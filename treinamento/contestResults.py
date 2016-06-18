89a3f0e4c75ad4733ef7d6c47df380558b40a798
hugocavalcanticabral <faz.sagarana@gmail.com>
2016-06-17 22:11:06 -0300

    update by shell

--- treinamento/contestResults.py
+++ treinamento/contestResults.py
@@ -1,8 +1,9 @@
 from sys import argv, exit
 import urllib
 import json
+import time
 
-PENALIDADE_WA = 20
+PENALIDADE_WA = 50
 
 usuarios = ["a.winston.s97", "hugo_cabral", "victoragnez", "RailtonT", "luizbarros", "tyronedamasceno", "heliobdf"]
 
@@ -15,23 +16,55 @@ intervalo = arquivo_contest.readline().split(' ')
 for line in arquivo_contest:
     questoes.append(line.rstrip())
 
+def points(p, ti, wa):
+	ti = ti - int(intervalo[0])
+	h = time.gmtime(ti)[3]
+	m = time.gmtime(ti)[4]
+	totalTime = h*60+m
+	if p<=500:
+		return max(150, p - 2*totalTime - PENALIDADE_WA*wa);
+	elif points<=1000:
+		return max(300, p - 4*totalTime - PENALIDADE_WA*wa);
+	elif points<=1500:
+		return max(450, p - 6*totalTime - PENALIDADE_WA*wa);
+	elif points<=2000:
+		return max(600, p - 8*totalTime - PENALIDADE_WA*wa);
+	else:
+		return max(750, p - 10*totalTime - PENALIDADE_WA*wa);
+
 for usuario in usuarios:
-    questoes_ja_contadas = []
-    url = "http://codeforces.com/api/user.status?handle=" + usuario + "&from=1&count=100";
+	url = "http://codeforces.com/api/user.status?handle=" + usuario + "&from=1&count=100";
     response = urllib.urlopen(url)
     data = json.loads(response.read())
-    if data['status'] == 'OK':
-        for submission in data['result']:
-            ti = submission['creationTimeSeconds']
+    
+    while data['status'] != 'OK':
+    	response = urllib.urlopen(url)
+    	data = json.loads(response.read())
+    
+	for question in questoes:
+		wrong = 0
+		acc = False
+		timeAcc = 0
+		point = 0
+		for submission in data['result']:
+			ti = submission['creationTimeSeconds']
             q = str(submission['problem']['contestId']) + submission['problem']['index']
-            if ti >= int(intervalo[0]) and ti <= int(intervalo[1]) and q in questoes:
-                if submission['verdict'] == 'OK' and q not in questoes_ja_contadas:
-                    p = submission['problem']['points']
-                    pontuacao[usuario] += p - (float(p)/300.0)*(ti - int(intervalo[0]))/60.0
-                    questoes_ja_contadas.append(q)
-
-                if submission['verdict'] == 'WRONG_ANSWER':
-                    pontuacao[usuario] -= PENALIDADE_WA
+            if ti >= int(intervalo[0]) and ti <= int(intervalo[1]) and q == question and submission['verdict'] == 'OK':
+            	point = submission['problem']['points']
+				if acc == False:
+					acc = True
+					timeAcc = ti
+				else:
+					timeAcc = min(timeAcc, ti)
+		
+		if acc == True:
+			for submission in data['result']:
+				ti = submission['creationTimeSeconds']
+		        q = str(submission['problem']['contestId']) + submission['problem']['index']
+		        if ti >= int(intervalo[0]) and ti <= int(intervalo[1]) and q == question:
+		        	if submission['verdict'] != 'OK' and ti < timeAcc:
+		        		wrong = wrong + 1
+            pontuacao[usuario] += points(point, timeAcc, wrong)
 
 for usuario in usuarios:
 	f = open("score " + argv[1] + ".txt", 'a')
